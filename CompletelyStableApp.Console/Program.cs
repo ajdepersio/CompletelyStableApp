a36a4e081cbddfecfd3e30bd1e69788b26e55ade
Andrew DePersio <ajdepersio@gmail.com>
2021-11-14 13:03:37 -0500

    Initial commit

---
+++ CompletelyStableApp.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using CompletelyStableApp.Lib;
+using System;
+
+namespace CompletelyStableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up!");
+            var timebomb = new Timebomb(30000);
+            timebomb.Start();
+            
+            var number = 1;
+            while (true)
+            {
+                var nextPrime = PrimeNumberFinder.FindNextPrimeNumber(number);
+                Console.WriteLine(nextPrime);
+                number = nextPrime + 1;
+            }
+        }
+    }
+}
---
+++ CompletelyStableApp.Lib/PrimeNumberFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CompletelyStableApp.Lib
+{
+    public static class PrimeNumberFinder
+    {
+        public static int FindNextPrimeNumber(int start)
+        {
+            while (!IsPrime(start))
+            {
+                start++;
+            }
+            return start;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            var limit = Math.Ceiling(Math.Sqrt(number)); //hoisting the loop limit
+
+            for (int i = 2; i <= limit; ++i)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
